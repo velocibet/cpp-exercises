673c87b6ec8b70f6a464c4e28db802efc4ab82b0
honoka4h <honoka4h@gmail.com>
2025-12-16 00:02:15 +0900

    Add GCD and LCM functions

--- GCD_LCM/main.cpp
+++ GCD_LCM/main.cpp
@@ -2,15 +2,7 @@
 #include <windows.h>
 using namespace std;
 
-int main() {
-    SetConsoleOutputCP(CP_UTF8);
-
-    int a, b;
-    cin >> a >> b;
-
-    int ia = a;
-    int ib = b;
-
+int getGCD(int a, int b) {
     int r;
     while (b != 0) {
         r = a % b;
@@ -18,8 +10,21 @@ int main() {
         b = r;
     }
 
-    cout << "최대공약수: " << a << endl;
-    cout << "최소공배수: " << ia * ib / a << endl;
+    return a;
+}
+
+int getLCM(int a, int b) {
+    return a * b / getGCD(a, b);
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << "최대공약수: " << getGCD(a, b) << endl;
+    cout << "최소공배수: " << getLCM(a, b) << endl;
 
     return 0;
 }
